74a51b051e1f33e76ce24795fd8c278d5f5f9c54
Taylor <tburke@power-innovations.com>
2020-07-30 15:05:48 -0600

    fix broken import

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import IChingDeck from './IChingDeck';
+import CardGenerator from "./CardGenerator";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <IChingDeck />
+        <CardGenerator />
       </header>
     </div>
   );
