be39c4c0f05264790582a382fb3ee9e663127c8c
Taylor <twillyb@gmail.com>
2020-08-29 20:40:13 -0600

    just rubbish

--- src/Components/DeckPresenter.tsx
+++ src/Components/DeckPresenter.tsx
@@ -1,22 +1,40 @@
 import DeckGenerator from "./DeckGenerator";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Deck} from "../interface/Deck/Deck";
+import {Action, Store, Dispatch} from "redux";
+import {connect} from "react-redux";
 
 type DeckPresenterProps = {
     selectedDecks: Deck[];
     stagedDeck: string;
+    // dispatch: Dispatch;
 }
 
 const DeckPresenter = ({selectedDecks, stagedDeck} : DeckPresenterProps) => {
 
     const handleClick = (deck : Deck) => {
-        // dispatch action to make the deck staged
+        alert("D")
     }
+
+    const [padding, setPadding] = useState(22);
+
+    useEffect(()=>{
+alert(`${padding}`)
+    }, [padding])
+
+    let hoverToggle = false;
+    const onHover = () => {
+        setPadding(padding+1)
+    }
+
+
+
     return (
         <div style={{display: 'flex', flexDirection: 'row'}}>
             {selectedDecks.map(d=>{
                 return (
-                    <div onClick={(e)=>handleClick(d)}>
+                    // the hover effects need to be moved to the dg component
+                    <div style={{marginTop: `${padding}`}} onMouseEnter={(e)=>onHover()} onClick={(e)=>handleClick(d)}>
                         <DeckGenerator isStaged={stagedDeck == d.name} deck={d}/>
                     </div>
                     )})
@@ -25,4 +43,16 @@ const DeckPresenter = ({selectedDecks, stagedDeck} : DeckPresenterProps) => {
     )
 }
 
-export default DeckPresenter;
+interface ConnectedDispatch {
+    stageDeck: (deck: Deck) => Action;
+}
+
+const mapDispatchToProps = (dispatch: Dispatch) : ConnectedDispatch => {
+    return {
+        stageDeck: (deck: Deck) => dispatch(stageDeck(deck))
+    }
+};
+
+// map dispatch to props
+const connectedDeckPresenter = connect(null, mapDispatchToProps)(DeckPresenter);
+export default connectedDeckPresenter;
---
+++ src/interface/Actions/ActionCreators.ts
@@ -0,0 +1,5 @@
+import {Deck} from "../Deck/Deck";
+
+const stageDeck = (deck : Deck) => {
+
+}
